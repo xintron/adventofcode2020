f68aec0ea14351bb4330a0d17375fb2740f08134
Marcus Carlsson <carlsson.marcus@gmail.com>
2020-12-13 18:43:05 +0100

    Day 13 - part 2
    Added num crate to calculate Least Common Multiple

--- src/day13.rs
+++ src/day13.rs
@@ -1,6 +1,6 @@
 pub struct Timetable {
     time: usize,
-    busses: Vec<usize>,
+    busses: Vec<Option<usize>>,
 }
 
 impl std::str::FromStr for Timetable {
@@ -15,8 +15,8 @@ impl std::str::FromStr for Timetable {
                 .next()
                 .map(|line| {
                     line.split(',')
-                        .filter_map(|x| x.parse().ok())
-                        .collect::<Vec<usize>>()
+                        .map(|x| x.parse().ok())
+                        .collect::<Vec<Option<usize>>>()
                 })
                 .unwrap(),
         })
@@ -25,17 +25,24 @@ impl std::str::FromStr for Timetable {
 
 impl Timetable {
     fn next_bus(&self) -> (usize, usize) {
-        let mut delta: usize = usize::MAX;
-        let mut next_bus: usize = self.busses[0];
+        let busses = self.busses.iter();
 
-        for bus in self.busses.to_owned() {
-            let d = bus - (self.time % bus);
-            if d < delta {
-                delta = d;
-                next_bus = bus;
-            }
-        }
-        (next_bus, delta)
+        busses
+            .scan((0, usize::MAX), |state, bus| {
+                match bus {
+                    Some(b) => {
+                        let delta = b - (self.time % b);
+                        if delta < state.1 {
+                            *state = (*b, delta)
+                        }
+                    }
+                    None => (),
+                }
+                Some(*state)
+            })
+            .last()
+            .unwrap()
+            .to_owned()
     }
 }
 
@@ -50,6 +57,45 @@ pub fn part1(timetable: &Timetable) -> usize {
     bus * delta
 }
 
+#[aoc(day13, part2)]
+pub fn part2(timetable: &Timetable) -> usize {
+    use num::Integer;
+
+    let b: Vec<(usize, usize)> = timetable
+        .busses
+        .clone()
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| x.is_some())
+        .map(|(i, x)| (i, x.unwrap()))
+        .collect();
+    let mut increment = b[0].1;
+    let mut time = 0usize;
+    let mut index_found: usize = 0;
+    loop {
+        time += increment;
+        let mut all = true;
+        for (offset, value) in &b {
+            let num = time + offset;
+            if num % value == 0 {
+                if *offset > index_found {
+                    increment = increment.lcm(value);
+                    index_found = *offset;
+                }
+                continue;
+            }
+            all &= false;
+            break;
+        }
+
+        // All matches, we're done
+        if all {
+            break;
+        }
+    }
+    time
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,7 +107,19 @@ mod tests {
     fn test_day13_input_generator() {
         let timetable: Timetable = input_generator(SAMPLE_INPUT);
         assert_eq!(timetable.time, 939);
-        assert_eq!(timetable.busses, vec![7, 13, 59, 31, 19]);
+        assert_eq!(
+            timetable.busses,
+            vec![
+                Some(7),
+                Some(13),
+                None,
+                None,
+                Some(59),
+                None,
+                Some(31),
+                Some(19)
+            ]
+        );
     }
 
     #[test]
@@ -69,5 +127,26 @@ mod tests {
         let timetable: Timetable = input_generator(SAMPLE_INPUT);
         assert_eq!(timetable.next_bus(), (59, 5));
         assert_eq!(part1(&timetable), 295);
+    }
+
+    #[test]
+    fn test_day13_part2() {
+        let timetable: Timetable = input_generator(SAMPLE_INPUT);
+        assert_eq!(part2(&timetable), 1068781);
+
+        let timetable: Timetable = input_generator("0\n17,x,13,19");
+        assert_eq!(part2(&timetable), 3417);
+
+        let timetable: Timetable = input_generator("0\n67,7,59,61");
+        assert_eq!(part2(&timetable), 754018);
+
+        let timetable: Timetable = input_generator("0\n67,x,7,59,61");
+        assert_eq!(part2(&timetable), 779210);
+
+        let timetable: Timetable = input_generator("0\n67,7,x,59,61");
+        assert_eq!(part2(&timetable), 1261476);
+
+        let timetable: Timetable = input_generator("0\n1789,37,47,1889");
+        assert_eq!(part2(&timetable), 1202161486);
     }
 }
